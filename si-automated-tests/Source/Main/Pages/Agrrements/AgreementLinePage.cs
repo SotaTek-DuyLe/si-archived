4977c98ab4854f526769dcbb09bfdd9cbcdb1b42
Trang Nguyen Thi <trang.nguyen4@sotatek.com>
2022-03-16 14:50:32 +0700

    feat: add new TC_015

--- si-automated-tests/Source/Main/Pages/Agrrements/AgreementLinePage.cs
+++ si-automated-tests/Source/Main/Pages/Agrrements/AgreementLinePage.cs
@@ -11,9 +11,15 @@ namespace si_automated_tests.Source.Main.Pages.Agrrements
     public class AgreementLinePage : BasePage
     {
         private readonly By allTabs = By.XPath("//a[@role='tab']");
+        private readonly By title = By.XPath("//h4[text()='AGREEMENTLINE']");
         private readonly By closeWithoutSavingBtn = By.XPath("//a[@aria-controls='details-tab']/ancestor::body//button[@title='Close Without Saving']");
 
         private const string frameMessage = "//div[@class='notifyjs-corner']/div";
+
+        //DYNAMIC LOCATOR
+        private const string titleContainsId = "//p[text()='Agreement ID {0}']";
+        private const string anyTab = "//a[text()='{0}']";
+
         public AgreementLinePage GoToAllTabAndConfirmNoError()
         {
             IList<IWebElement> elements = WaitUtil.WaitForAllElementsVisible(allTabs);
@@ -30,6 +36,19 @@ namespace si_automated_tests.Source.Main.Pages.Agrrements
         {
             WaitUtil.WaitForElementClickable(closeWithoutSavingBtn);
             ClickOnElement(closeWithoutSavingBtn);
+            return this;
+        }
+
+        public AgreementLinePage WaitForWindowLoadedSuccess(string id)
+        {
+            WaitUtil.WaitForElementVisible(title);
+            WaitUtil.WaitForElementVisible(string.Format(titleContainsId, id));
+            return this;
+        }
+
+        public AgreementLinePage ClickDetailTab()
+        {
+            ClickOnElement(string.Format(anyTab, "Details"));
             return this;
         }
     }
--- si-automated-tests/Source/Main/Pages/NavigationPanel/PartyNavigation.cs
+++ si-automated-tests/Source/Main/Pages/NavigationPanel/PartyNavigation.cs
@@ -9,11 +9,13 @@ namespace si_automated_tests.Source.Main.Pages.Paties
         private readonly string partiesSubSubMenu = "//span[text()='Parties']/parent::a";
         private readonly string agreementSubSubMenu = "//span[text()='Agreements']/parent::a";
         private readonly string siteServiceSubSubMenu = "//span[text()='Site Services']/parent::a";
+        private readonly string titleHomePage = "//p[text()='Northstar Environmental Services – Demo 8.6.0-dev01']";
 
         
         public PartyNavigation ClickPartySubMenu()
         {
             ClickOnElement(partiesSubSubMenu);
+            ClickOnElement(titleHomePage);
             return this;
         }
         public PartyNavigation ClickAgreementSubMenu()
