3885ee936bc08f63f43d988927b68fea9ba8cc1b
Dee <duy.le@sotatek.com>
2022-03-31 23:32:32 +0700

    modify url variable

--- si-automated-tests/Source/Main/Pages/NavigationPanel/NavigationBase.cs
+++ si-automated-tests/Source/Main/Pages/NavigationPanel/NavigationBase.cs
@@ -19,7 +19,6 @@ namespace si_automated_tests.Source.Main.Pages.NavigationPanel
         private readonly string mainOption = "//span[text()='{0}']/parent::h4/parent::div";
         private readonly string dropdownOption = "//span[text()='{0}']/parent::a/preceding-sibling::span[2]";
         private readonly string option = "//span[text()='{0}']/parent::a";
-        private readonly string locatorContainsExpanded = "//span[text()='North Star Commercial']/parent::a/preceding-sibling::span[contains(@class, 'expanded')]";
 
 
         public NavigationBase ClickMainOption(string optionName)
