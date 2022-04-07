92f6f8647ba4e214af862a323be2feca489667ec
dee <duy.le@sotatek.com>
2022-04-07 17:21:42 +0700

    add tc 71

--- si-automated-tests/Source/Test/ResourcesTests/LeaveEntryTests.cs
+++ si-automated-tests/Source/Test/ResourcesTests/LeaveEntryTests.cs
@@ -116,7 +116,7 @@ namespace si_automated_tests.Source.Test.ResourcesTests
                 .VerifyFirstResultValue("Verdict", "Approved");
         }
         [Category("Resources")]
-        [Test, Order(2)]
+        [Test]
         public void TC_69()
         {
             string currentDate = CommonUtil.GetLocalTimeNow("dd/MM/yyyy");
@@ -209,7 +209,7 @@ namespace si_automated_tests.Source.Test.ResourcesTests
                 .VerifyFirstResultValue("Verdict", "Declined");
         }
         [Category("Resources")]
-        [Test, Order(2)]
+        [Test]
         public void TC_70()
         {
             string currentDate = CommonUtil.GetLocalTimeNow("dd/MM/yyyy");
@@ -283,7 +283,98 @@ namespace si_automated_tests.Source.Test.ResourcesTests
                 .OpenFirstResult()
                 .SwitchToLastWindow()
                 .WaitForLoadingIconToDisappear();
+
             //TC-69: DELETE LEAVE ENTRY
+            PageFactoryManager.Get<LeaveEntryPage>()
+                .IsOnLeaveEntryPage()
+                .DeleteLeaveEntry()
+                .SwitchToLastWindow();
+            PageFactoryManager.Get<LeaveEntryPage>()
+                .ConfirmDeleteLeaveEntry()
+                .WaitForLoadingIconToDisappear()
+                .SleepTimeInMiliseconds(2500) //wait for window to be automatically closed
+                .SwitchToLastWindow()
+                .SwitchNewIFrame();
+            PageFactoryManager.Get<CommonBrowsePage>()
+                .ClickRefreshBtn()
+                .WaitForLoadingIconToDisappear();
+            PageFactoryManager.Get<CommonBrowsePage>()
+                .VerifyFirstResultValue("Resource", resourceName)
+                .VerifyFirstResultValue("Verdict", "Cancelled");
+        }
+        [Category("Resources")]
+        [Test]
+        public void TC_71()
+        {
+            string currentDate = CommonUtil.GetLocalTimeNow("dd/MM/yyyy");
+            string details = CommonUtil.GetRandomString(5);
+            string resourceName = "Neil Armstrong " + CommonUtil.GetRandomNumber(5);
+            string resourceType = "Driver";
+            string leaveType = "Jury Service";
+
+            PageFactoryManager.Get<LoginPage>()
+                .GoToURL(WebUrl.MainPageUrl);
+            PageFactoryManager.Get<LoginPage>()
+                .IsOnLoginPage()
+                .Login(AutoUser4.UserName, AutoUser4.Password)
+                .IsOnHomePage(AutoUser4);
+            PageFactoryManager.Get<NavigationBase>()
+                .ClickMainOption("Resources")
+                .OpenOption("Default Allocation")
+                .SwitchNewIFrame();
+            PageFactoryManager.Get<ResourceAllocationPage>()
+                .SelectContract("North Star Commercial")
+                .SelectBusinessUnit("North Star Commercial")
+                .SelectShift("AM")
+                .ClickGo()
+                .WaitForLoadingIconToDisappear()
+                .SleepTimeInMiliseconds(2000);
+            //CREATE NEW RESOURCE
+            PageFactoryManager.Get<ResourceAllocationPage>()
+                .ClickCreateResource()
+                .SwitchToLastWindow()
+                .WaitForLoadingIconToDisappear();
+            PageFactoryManager.Get<ResourceDetailTab>()
+                .IsOnDetailTab()
+                .InputResourceName(resourceName)
+                .SelectResourceType(resourceType)
+                .TickContractRoam()
+                .ClickSaveBtn()
+                .VerifyToastMessage("Successfully saved resource.")
+                .ClickCloseBtn()
+                .SwitchToLastWindow();
+
+            PageFactoryManager.Get<NavigationBase>()
+                .ClickMainOption("Resources")
+                .ExpandOption("North Star Commercial")
+                .OpenOption("Leave Entry")
+                .SwitchNewIFrame();
+            PageFactoryManager.Get<CommonBrowsePage>()
+                .ClickButton("Create Leave Entry Record")
+                .SwitchToLastWindow()
+                .WaitForLoadingIconToDisappear();
+            PageFactoryManager.Get<LeaveEntryPage>()
+                .IsOnLeaveEntryPage()
+                .SelectLeaveResource(resourceName)
+                .SelectLeaveType(leaveType)
+                .EnterDates(currentDate)
+                .EnterDetails(details)
+                .SaveLeaveEntry()
+                .VerifyToastMessage("Successfully saved Leave Entry")
+                .CloseCurrentWindow()
+                .SwitchToLastWindow()
+                .SwitchNewIFrame();
+            PageFactoryManager.Get<CommonBrowsePage>()
+                .ClickRefreshBtn()
+                .WaitForLoadingIconToDisappear();
+            PageFactoryManager.Get<CommonBrowsePage>()
+                .VerifyFirstResultValue("Resource", resourceName)
+                .VerifyFirstResultValue("Verdict", "N/A")
+                .OpenFirstResult()
+                .SwitchToLastWindow()
+                .WaitForLoadingIconToDisappear();
+
+            //VERIFY LEAVE ENTRY CAN BE DELETED
             PageFactoryManager.Get<LeaveEntryPage>()
                 .IsOnLeaveEntryPage()
                 .DeleteLeaveEntry()
