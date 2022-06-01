90dbc3dd3c998901cdf506dfb9343c8b6690a83a
dee <duy.le@sotatek.com>
2022-06-01 18:14:20 +0700

    add tc 121

---
+++ si-automated-tests/Source/Main/Pages/Services/ContractSiteDetailPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using si_automated_tests.Source.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace si_automated_tests.Source.Main.Pages.Services
+{
+    class ContractSiteDetailPage :BasePage
+    {
+        private readonly By addBtn = By.XPath("//button[text()='Add']");
+        private readonly By searchBox = By.XPath("//input[@type='search']");
+        private readonly String addressOption = "//li[@class='list-group-item' and contains(text(),'{0}')]";
+
+
+        public ContractSiteDetailPage ClickAddBtn()
+        {
+            ClickOnElement(addBtn);
+            return this;
+        }
+        public ContractSiteDetailPage SearchForAddress(string add)
+        {
+            SendKeys(searchBox, add);
+            return this;
+        }
+        public ContractSiteDetailPage SelectAddress(string add)
+        {
+            ClickOnElement(addressOption, add);
+            return this;
+        }
+    }
+}
