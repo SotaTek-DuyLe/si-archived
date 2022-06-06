e856fd6fd42a945fd3be86116e59ee47769b5720
TrangNguyen <trang.nguyen4@sotatek.com>
2022-06-06 11:31:46 +0700

    temp commit

---
+++ si-automated-tests/Source/Main/Pages/Applications/ServiceDataManagementPage.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using si_automated_tests.Source.Core;
+using si_automated_tests.Source.Core.WebElements;
+using si_automated_tests.Source.Main.Constants;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace si_automated_tests.Source.Main.Pages.Applications
+{
+    public class ServiceDataManagementPage : BasePageCommonActions
+    {
+        public readonly By ServiceLocationTypeSelect = By.XPath("//div[@id='screen1']//select[@id='type']");
+        public readonly By NextButton = By.XPath("//div[@id='screen1']//button[@id='next-button']");
+        public readonly By PreviousButton = By.XPath("//div[@id='screen2']//button[contains(string(), 'Previous')]");
+        public readonly By ApplyButton = By.XPath("//div[@id='screen2']//button[contains(string(), 'Apply')]");
+        public readonly By TotalSpan = By.XPath("//div[@id='screen2']//div[contains(@class, 'south-panel2')]//span");
+        /// <summary>
+        /// SDM: Service Data Management
+        /// </summary>
+        private readonly string SDMTableXPath = "//div[@id='screen1']//div[@class='grid-canvas']";
+        private readonly string SDMRowXPath = "./div[contains(@class, 'slick-row')]";
+        private readonly string SDMCheckboxXPath = "./div[contains(@class, 'slick-cell l0')]//input[@type='checkbox']";
+        private readonly string SDMPointAddressXPath = "./div[contains(@class, 'slick-cell l1')]";
+        private readonly string SDMTypeXPath = "./div[contains(@class, 'slick-cell l2')]";
+        private readonly string SDMDescriptionXPath = "./div[contains(@class, 'slick-cell l3')]";
+        private readonly string SDMPostcodeXPath = "./div[contains(@class, 'slick-cell l4')]";
+        private readonly string SDMStreetXPath = "./div[contains(@class, 'slick-cell l5')]";
+        private readonly string SDMStatusXPath = "./div[contains(@class, 'slick-cell l6')]";
+
+
+        public TableElement ServiceDataManagementTableElement
+        {
+            get
+            {
+                return new TableElement(
+                        SDMTableXPath,
+                        SDMRowXPath, 
+                        new List<string>(){ SDMCheckboxXPath, SDMPointAddressXPath, SDMTypeXPath, SDMDescriptionXPath, SDMPostcodeXPath, SDMStreetXPath, SDMStatusXPath });
+            }
+        }
+
+        public ServiceDataManagementPage ClickPointAddress(string poinAddress)
+        {
+            ClickOnElement(ServiceDataManagementTableElement.GetCellByValue(1, poinAddress));
+            return this;
+        }
+
+        public ServiceDataManagementPage ClickMultiPointAddress(int rowCount)
+        {
+            for (int i = 0; i < rowCount; i++)
+            {
+                ServiceDataManagementTableElement.ClickCell(i, 0);
+            }
+            return this;
+        }
+    }
+}
---
+++ si-automated-tests/Source/Test/ApplicationTests/ServiceDataManagamentTest.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using si_automated_tests.Source.Core;
+using si_automated_tests.Source.Main.Constants;
+using si_automated_tests.Source.Main.Models;
+using si_automated_tests.Source.Main.Pages;
+using si_automated_tests.Source.Main.Pages.Applications;
+using si_automated_tests.Source.Main.Pages.NavigationPanel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static si_automated_tests.Source.Main.Models.UserRegistry;
+
+namespace si_automated_tests.Source.Test.ApplicationTests
+{
+    [Parallelizable(scope: ParallelScope.Fixtures)]
+    [TestFixture]
+    public class ServiceDataManagamentTest : BaseTest
+    {
+        [Category("TC_122_1 Verify that the layout of the new Service Data Management screen appears correctly")]
+        [Test]
+        public void TC_122_1_Verify_Layout_Service_Data_Management()
+        {
+            PageFactoryManager.Get<LoginPage>()
+                .GoToURL(WebUrl.MainPageUrl);
+            PageFactoryManager.Get<LoginPage>()
+                .IsOnLoginPage()
+                .Login(AutoUser38.UserName, AutoUser38.Password)
+                .IsOnHomePage(AutoUser38);
+            PageFactoryManager.Get<NavigationBase>()
+                .ClickMainOption("Applications")
+                .OpenOption("Service Data Management")
+                .SwitchNewIFrame();
+
+            ServiceDataManagementPage serviceDataManagementPage = PageFactoryManager.Get<ServiceDataManagementPage>();
+            serviceDataManagementPage
+                 .WaitForLoadingIconToDisappear();
+            serviceDataManagementPage
+                .SelectTextFromDropDown(serviceDataManagementPage.ServiceLocationTypeSelect, "Address")
+                .WaitForLoadingIconToDisappear();
+            serviceDataManagementPage
+                .ClickPointAddress("5")
+                .ClickOnElement(serviceDataManagementPage.NextButton);
+            serviceDataManagementPage.WaitForLoadingIconToDisappear();
+            serviceDataManagementPage
+                .VerifyElementVisibility(serviceDataManagementPage.PreviousButton, true)
+                .VerifyElementVisibility(serviceDataManagementPage.ApplyButton, true)
+                .VerifyElementText(serviceDataManagementPage.TotalSpan, "Total = 1", true);
+        }
+
+        [Category("TC_122_2 Verify that every selected Points appears in Service Data Management screen ")]
+        [Test]
+        public void TC_122_2_Verify_Every_Selected_Points_Appears_In_Service_Data_Management()
+        {
+            PageFactoryManager.Get<LoginPage>()
+                .GoToURL(WebUrl.MainPageUrl);
+            PageFactoryManager.Get<LoginPage>()
+                .IsOnLoginPage()
+                .Login(AutoUser38.UserName, AutoUser38.Password)
+                .IsOnHomePage(AutoUser38);
+            PageFactoryManager.Get<NavigationBase>()
+                .ClickMainOption("Applications")
+                .OpenOption("Service Data Management")
+                .SwitchNewIFrame();
+
+            ServiceDataManagementPage serviceDataManagementPage = PageFactoryManager.Get<ServiceDataManagementPage>();
+            int rowCount = 10;
+            serviceDataManagementPage
+                 .WaitForLoadingIconToDisappear();
+            serviceDataManagementPage
+                .SelectTextFromDropDown(serviceDataManagementPage.ServiceLocationTypeSelect, "Address")
+                .WaitForLoadingIconToDisappear();
+            serviceDataManagementPage
+                .ClickMultiPointAddress(rowCount);
+            serviceDataManagementPage.WaitForLoadingIconToDisappear();
+            serviceDataManagementPage
+                .VerifyElementText(serviceDataManagementPage.TotalSpan, $"Total = {rowCount}", true);
+        }
+    }
+}
