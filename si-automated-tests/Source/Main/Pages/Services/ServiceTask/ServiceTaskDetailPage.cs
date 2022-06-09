35e8e87cc5868b5e02f6f7da675ea98097ef5d08
Trang Nguyen Thi <leyla.trangnguyen@setel.com>
2022-06-09 13:53:46 +0700

    no message

---
+++ si-automated-tests/Source/Main/Pages/Services/ServiceTask/ServiceTaskDetailPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenQA.Selenium;
+using si_automated_tests.Source.Core;
+
+namespace si_automated_tests.Source.Main.Pages.Services.ServiceTask
+{
+    public class ServiceTaskDetailPage : BasePage
+    {
+        private readonly By titleServiceTaskDetail = By.XPath("//span[text()='Service Task']");
+
+        public ServiceTaskDetailPage WaitForServiceTaskDetailPageDisplayed()
+        {
+            WaitUtil.WaitForElementVisible(titleServiceTaskDetail);
+            return this;
+        }
+    }
+}
