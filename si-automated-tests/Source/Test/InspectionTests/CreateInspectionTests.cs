0ffd9cdd9ef07bec96b0022005cb9863a5cd115e
Trang Nguyen Thi <leyla.trangnguyen@setel.com>
2022-07-12 16:31:09 +0700

    update scripts with new version

--- si-automated-tests/Source/Test/InspectionTests/CreateInspectionTests.cs
+++ si-automated-tests/Source/Test/InspectionTests/CreateInspectionTests.cs
@@ -442,7 +442,7 @@ namespace si_automated_tests.Source.Test.InspectionTests
             List<InspectionDBModel> inspections = finder.GetInspectionById(inspectionId);
 
             PageFactoryManager.Get<DetailInspectionPage>()
-                .VerifyDataDisplayedWithDB(inspections[0], noteValue, 5, 0, 2, CommonUtil.GetLocalTimeNow(CommonConstants.DATE_MM_DD_YYYY_FORMAT), CommonUtil.GetLocalTimeMinusDay(CommonConstants.DATE_MM_DD_YYYY_FORMAT, 1))
+                .VerifyDataDisplayedWithDB(inspections[0], noteValue, 5, 0, 2, CommonUtil.GetLocalTimeNow(CommonConstants.DATE_DD_MM_YYYY_FORMAT), CommonUtil.GetLocalTimeMinusDay(CommonConstants.DATE_DD_MM_YYYY_FORMAT, 1), CommonUtil.GetLocalTimeNow(CommonConstants.DATE_MM_DD_YYYY_FORMAT), CommonUtil.GetLocalTimeMinusDay(CommonConstants.DATE_MM_DD_YYYY_FORMAT, 1))
                 .ClickCloseBtn()
                 .SwitchToChildWindow(3);
             PageFactoryManager.Get<ServiceUnitDetailPage>()
