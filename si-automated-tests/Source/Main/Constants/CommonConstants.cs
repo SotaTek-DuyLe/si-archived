cde156f9cf76a86a76eac34a53b7a3b45e3a0554
Trang Nguyen Thi <leyla.trangnguyen@setel.com>
2022-08-09 14:29:46 +0700

    change data to test

--- si-automated-tests/Source/Main/Constants/CommonConstants.cs
+++ si-automated-tests/Source/Main/Constants/CommonConstants.cs
@@ -42,7 +42,7 @@ namespace si_automated_tests.Source.Main.Constants
         public static readonly string[] ColumnInFindServiceUnitWindow = { "ID", "Service Unit", "Point Count", "Select" };        public static readonly string[] ServiceUpdateColumnHistoryTab = { "ActualAssetQuantity", "ActualProductQuantity", "State", "Resolution Code", "Completed Date", "Auto Confirmed" };
         public static readonly string[] ServiceUpdateColumnNotCompletedHistoryTab = { "ActualAssetQuantity", "ActualProductQuantity", "State", "Completed Date", "Auto Confirmed" };        public static readonly string[] UpdateColumnHistoryTabFirst = { "Task notes", "Completed date", "State", "End date", "Resolution code" };
         public static readonly string[] UpdateColumnHistoryTabSecond = { "Task notes", "Completed date", "State", "End date" };
-
+        public static readonly string[] ServiceUpdateTaskLineFromCancelledToNotCompleted = { "State", "Completed Date", "Auto Confirmed" };        
         //DATE
         public static readonly string DATE_DD_MM_YYYY_FORMAT = "dd/MM/yyyy";
         public static readonly string DATE_DD_MM_YYYY_FORMAT_DB = "dd-MM-yyyy";
