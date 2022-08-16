a722976d7e8a436146531d335cad7305b62b9efc
dee <duy.le@sotatek.com>
2022-08-16 14:49:13 +0700

    add main option file

---
+++ si-automated-tests/Source/Main/Constants/MainOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace si_automated_tests.Source.Main.Constants
+{
+    public class MainOption
+    {
+        public static readonly string Applications = "Applications";
+        public static readonly string Parties = "Parties";
+        public static readonly string Accounts = "Accounts";
+        public static readonly string Services = "Services";
+        public static readonly string Events = "Events";
+        public static readonly string Tasks = "Tasks";
+        public static readonly string Inspections = "Inspections";
+        public static readonly string Resources = "Resources";
+        public static readonly string Maps = "Maps";
+        public static readonly string Weighbridge = "Weighbridge";
+        public static readonly string Reports = "Reports";
+    }
+}
