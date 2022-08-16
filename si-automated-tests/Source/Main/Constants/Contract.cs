5554917fd71df0472ef4e348c36bd660e99d75ba
dee <duy.le@sotatek.com>
2022-08-16 10:12:45 +0700

    change value of contract var

--- si-automated-tests/Source/Main/Constants/Contract.cs
+++ si-automated-tests/Source/Main/Constants/Contract.cs
@@ -3,8 +3,8 @@ namespace si_automated_tests.Source.Main.Constants
 {
 	public class Contract
 	{
-		public static readonly string NS = "North Star";
-		public static readonly string NSC = "North Star Commercial";
+		public static readonly string NS = "Richmond";
+		public static readonly string NSC = "Richmond Commercial";
 		public static readonly string SYD = "Sydney";
 	}
 }
