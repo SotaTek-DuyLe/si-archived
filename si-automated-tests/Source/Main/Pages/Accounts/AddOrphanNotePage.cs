c3e50175208e9673e34a64d4024bc1bd3c50a4f6
TrangNguyen <trang.nguyen4@sotatek.com>
2022-12-08 09:22:03 +0700

    refactor: rename variables

--- si-automated-tests/Source/Main/Pages/Accounts/AddOrphanNotePage.cs
+++ si-automated-tests/Source/Main/Pages/Accounts/AddOrphanNotePage.cs
@@ -12,19 +12,19 @@ namespace si_automated_tests.Source.Main.Pages.Accounts
     {
         public readonly By ConfirmButton = By.XPath("//button[text()='Confirm']");
         public readonly By CancelButton = By.XPath("//button[text()='Cancel']");
-        private string CreditNoteTable = "//div[@id='add-orphan-notes']//tbody[@data-bind='foreach: creditNotes']";
-        private string CreditNoteRow = "./tr";
-        private string CheckboxCell = "./td//input";
-        private string NoteIdCell = "./td[@data-bind='text: noteId']";
-        private string CreditDateCell = "./td[@data-bind='text: creditDate']";
-        private string PartyCell = "./td[@data-bind='text: party']";
-        private string AccountCell = "./td[@data-bind='text: accountRef']";
-        private string AccountNumberCell = "./td[@data-bind='text: accountNumber']";
-        private string NetCell = "./td[@data-bind='text: net']";
+        private string creditNoteTable = "//div[@id='add-orphan-notes']//tbody[@data-bind='foreach: creditNotes']";
+        private string creditNoteRow = "./tr";
+        private string checkboxCell = "./td//input";
+        private string noteIdCell = "./td[@data-bind='text: noteId']";
+        private string creditDateCell = "./td[@data-bind='text: creditDate']";
+        private string partyCell = "./td[@data-bind='text: party']";
+        private string accountCell = "./td[@data-bind='text: accountRef']";
+        private string accountNumberCell = "./td[@data-bind='text: accountNumber']";
+        private string netCell = "./td[@data-bind='text: net']";
 
         public TableElement CreditNoteTableEle
         {
-            get => new TableElement(CreditNoteTable, CreditNoteRow, new System.Collections.Generic.List<string>() { CheckboxCell, NoteIdCell, CreditDateCell, PartyCell, AccountCell, AccountNumberCell, NetCell });
+            get => new TableElement(creditNoteTable, creditNoteRow, new System.Collections.Generic.List<string>() { checkboxCell, noteIdCell, creditDateCell, partyCell, accountCell, accountNumberCell, netCell });
         }
 
         public AddOrphanNotePage SelectCreditNote()
@@ -35,7 +35,7 @@ namespace si_automated_tests.Source.Main.Pages.Accounts
 
         public AddOrphanNotePage VerifyNetValueHasValueGreaterThanZero()
         {
-            double netVal = CreditNoteTableEle.GetCellValue(0, CreditNoteTableEle.GetCellIndex(NetCell)).AsDouble();
+            double netVal = CreditNoteTableEle.GetCellValue(0, CreditNoteTableEle.GetCellIndex(netCell)).AsDouble();
             Assert.IsTrue(netVal > 0);
             return this;
         }
