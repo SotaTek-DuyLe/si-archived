a8f3816a2c09a84161ef583a56a0173cbc50dae3
TrangNguyen <trang.nguyen4@sotatek.com>
2023-02-22 16:02:43 +0700

    fix: tc_102

--- si-automated-tests/Source/Main/Pages/Accounts/DetailReceiptPage.cs
+++ si-automated-tests/Source/Main/Pages/Accounts/DetailReceiptPage.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using si_automated_tests.Source.Core;
+using si_automated_tests.Source.Core.WebElements;
 using si_automated_tests.Source.Main.Constants;
 using si_automated_tests.Source.Main.Models.DetailReceipt;
 using System;
@@ -11,10 +12,31 @@ using System.Threading;
 
 namespace si_automated_tests.Source.Main.Pages.Accounts
 {
-    public class DetailReceiptPage : BasePage
+    public class DetailReceiptPage : BasePageCommonActions
     {
         private readonly By addnewItemBtn = By.XPath("//button[contains(string(), 'Add New Item')]");
         private readonly By receiptRows = By.XPath("//div[@class='slick-viewport']//div[@class='grid-canvas']//div[contains(@class,'ui-widget-content')]");
+
+        private string ReceiptTable = "//div[@class='grid-canvas']";
+        private string ReceiptRow = "./div[contains(@class, 'slick-row')]";
+        private string ReceiptCheckboxCell = "./div[contains(@class, 'slick-cell l0 r0')]//input";
+        private string ReceiptPartyCell = "./div[contains(@class, 'slick-cell l3 r3')]";
+        private string ReceiptPaymentMethodCell = "./div[contains(@class, 'slick-cell l4 r4')]";
+        private string PaymentRefCell = "./div[contains(@class, 'slick-cell l5 r5')]";
+        public TableElement ReceiptTableEle
+        {
+            get => new TableElement(ReceiptTable, ReceiptRow, new List<string> { ReceiptCheckboxCell, ReceiptPartyCell, ReceiptPaymentMethodCell, PaymentRefCell });
+        }
+
+        [AllureStep]
+        public DetailReceiptPage VerifyReceipt(string party, string paymentMethod, string paymentRef)
+        {
+            VerifyCellValue(ReceiptTableEle, 0, ReceiptTableEle.GetCellIndex(ReceiptPartyCell), party);
+            VerifyCellValue(ReceiptTableEle, 0, ReceiptTableEle.GetCellIndex(ReceiptPaymentMethodCell), paymentMethod);
+            VerifyCellValue(ReceiptTableEle, 0, ReceiptTableEle.GetCellIndex(PaymentRefCell), paymentRef);
+            return this;
+        }
+
         [AllureStep]
         public DetailReceiptPage ClickAddNewItem()
         {
