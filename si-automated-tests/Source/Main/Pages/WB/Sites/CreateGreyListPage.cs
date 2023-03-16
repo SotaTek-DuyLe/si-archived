02fab76c454ef9e3fd0b3d9911312866bc4a1b81
Trang Nguyen Thi <leyla.trangnguyen@setel.com>
2023-03-16 13:46:24 +0700

    no message

--- si-automated-tests/Source/Main/Pages/WB/Sites/CreateGreyListPage.cs
+++ si-automated-tests/Source/Main/Pages/WB/Sites/CreateGreyListPage.cs
@@ -17,7 +17,7 @@ namespace si_automated_tests.Source.Main.Pages.WB.Sites
         private readonly By vehicleInput = By.XPath("//label[text()='Vehicle']/following-sibling::div//input");
         private readonly By startDateInput = By.CssSelector("input[id='start-date']");
         private readonly By endDateInput = By.CssSelector("input[id='end-date']");
-        private readonly By ticketDd = By.XPath("//label[text()='Ticket']/following-sibling::div//button/parent::div");
+        private readonly By ticketDd = By.XPath("//label[text()='Ticket']/following-sibling::div//button");
         private readonly By ticketBtn = By.XPath("//label[text()='Ticket']/following-sibling::div//button");
         private readonly By ticketButton = By.XPath("//label[text()='Ticket']/following-sibling::div//button/span[1]");
         private readonly By customerDd = By.CssSelector("select[id='customer']");
