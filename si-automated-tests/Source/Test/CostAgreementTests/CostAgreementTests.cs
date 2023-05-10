451cda7e6cfbac0ba2435b62229266af2ac277ea
Trang Nguyen Thi <leyla.trangnguyen@setel.com>
2023-05-10 09:54:36 +0700

    add TC212

---
+++ si-automated-tests/Source/Test/CostAgreementTests/CostAgreementTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using si_automated_tests.Source.Core;
+using si_automated_tests.Source.Main.Constants;
+using si_automated_tests.Source.Main.Pages;
+using static si_automated_tests.Source.Main.Models.UserRegistry;
+
+namespace si_automated_tests.Source.Test.CostAgreementTests
+{
+    [Parallelizable(scope: ParallelScope.Fixtures)]
+    [TestFixture]
+    public class CostAgreementTests : BaseTest
+    {
+        [Category("Cost agreement")]
+        [Category("Chang")]
+        [Test(Description = "Cost agreement - phase 2")]
+        public void TC_212_Cost_agreement_phase_2()
+        {
+            PageFactoryManager.Get<LoginPage>()
+                .GoToURL(WebUrl.MainPageUrl);
+            PageFactoryManager.Get<LoginPage>()
+                .IsOnLoginPage()
+                .Login(AutoUser14.UserName, AutoUser14.Password)
+                .IsOnHomePage(AutoUser14);
+        }
+    }
+}
