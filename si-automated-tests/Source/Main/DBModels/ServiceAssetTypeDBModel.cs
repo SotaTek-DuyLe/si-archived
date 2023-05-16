2c1e54bf596ad05da1312928419860d6d1931665
Huong Nguyen <huong.nguyen3@sotatek.com>
2023-05-16 21:32:33 +0700

    feat: tc_316

---
+++ si-automated-tests/Source/Main/DBModels/ServiceAssetTypeDBModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace si_automated_tests.Source.Main.DBModels
+{
+    public class ServiceAssetTypeDBModel
+    {
+        public int serviceunitID { get; set; }
+        public string assettype { get; set; }
+    }
+}
