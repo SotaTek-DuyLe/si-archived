97228aa629fbee4c5e876a2a1ea9bc0d4b1c1551
dee <duy.le@sotatek.com>
2023-07-28 08:55:30 +0700

    fixes

--- si-automated-tests/Source/Main/Constants/CommonConstants.cs
+++ si-automated-tests/Source/Main/Constants/CommonConstants.cs
@@ -67,7 +67,7 @@ namespace si_automated_tests.Source.Main.Constants
         public static readonly string[] AssociateObjectContractUnits = { "No associated objects" };
         public static readonly string[] AssociateObjectContractSite = { "No associated objects" };
         public static readonly string[] AssociateObjectRegion = { "No associated objects" };
-        public static readonly string[] AssociateObjectResource = { "Associated objects", "Round Resource Allocation", "Resource Qualification", "Round Group Resource Allocation" };
+        public static readonly string[] AssociateObjectResource = { "Associated objects", "Round Resource Allocation", "Round Group Resource Allocation" };
         public static readonly string[] AssociateObjectSite = { "Associated objects", "AgreementLine", "Service Unit Asset", "Agreement Line Task Type", "Service Task", "Service Task Schedule", "Service Task Line", "Agreement Line TaskTypeTaskLine", "Agreement Line Asset Product", "Service Unit", "Service Unit Point" };
         public static readonly string[] AssociateObjectAgreement = { "Service Task", "Service Task Schedule", "Service Task Line", "AgreementLine", "Service Unit Asset", "Agreement Line Task Type", "Agreement Line TaskTypeTaskLine", "Agreement Line Asset Product" };
         public static readonly string[] HistoryRIEvent = { "Object type", "Object ID", "Waste Type" };
